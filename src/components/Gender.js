27461fad518dfb6f13426339bad43c2861abc21b
iwetta <iwetta.majewska@gmail.com>
2022-11-27 13:44:16 +0100

    changes

--- src/components/Gender.js
+++ src/components/Gender.js
@@ -3,7 +3,12 @@ import Option from "./Option";
 const Gender = () => {
   return (
     <>
-      <select className="select" placeholder="płeć" required name="gender">
+      <select
+        className="select gender-select"
+        placeholder="płeć"
+        required
+        name="gender"
+      >
         <Option hidden={true} value={""} optionText="Wybierz płeć" />
         <Option sex={"female"} value={-161} optionText={"kobieta"} required />
         <Option sex={"male"} value={5} optionText={"mężczyzna"} required />
